248724cfa409826f5e0a357f2a6d606916b53dfa
Herman verschooten <Herman@verschooten.net>
2013-05-25 21:19:46 +0200

    Put rescue block around the handling

--- lib/cookieless/middleware.rb
+++ lib/cookieless/middleware.rb
@@ -22,23 +22,27 @@ module Rack
 
           status, header, response = @app.call(env)
 
-          if page_warrants_cookie?
-            session_id ||= env["rack.session"]["session_id"]
-            cache_cookie_by_session_id(session_id, header["Set-Cookie"])
-            fix_url(header["Location"],session_id)
-            if process_page?(header)
-              if page_has_body?(response)
-                if content_is_arrayed?(response.body)
-                  process_body(response.body[0],session_id)
+          begin
+            if page_warrants_cookie?
+              session_id ||= env["rack.session"]["session_id"]
+              cache_cookie_by_session_id(session_id, header["Set-Cookie"])
+              fix_url(header["Location"],session_id)
+              if process_page?(header)
+                if page_has_body?(response)
+                  if content_is_arrayed?(response.body)
+                    process_body(response.body[0],session_id)
+                  else
+                    process_body(response.body,session_id)
+                  end
                 else
-                  process_body(response.body,session_id)
-                end
-              else
-                if content_is_arrayed?(response)
-                  process_body(response[0],session_id)
+                  if content_is_arrayed?(response)
+                    process_body(response[0],session_id)
+                  end
                 end
               end
             end
+          rescue Exception => e
+            Rails.logger.info "** Unhandled Exception in Cookieless::Middleware **\n" + e.message + "\n" + e.backtrace.join("\n")
           end
           [status, header, response]
         end
